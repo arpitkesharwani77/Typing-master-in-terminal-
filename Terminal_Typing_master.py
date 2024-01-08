22dcdb5ff989eda7bdb9c2dc66d12838a6762932
arpitkesharwani77 <arpitkesharwani77@gmail.com>
2024-01-08 15:43:11 +0530

    taking user input

--- Terminal_Typing_master.py
+++ Terminal_Typing_master.py
@@ -54,3 +54,21 @@ def show_leaderboard():
     else:
         for user, metrics in leaderboard.items():
             print(f"{user}: {metrics}")
+# Get user input    
+def get_user_input():
+  try:
+    username = input(f"{YELLOW}Enter your Username: {END_COLOR} ").capitalize()
+    print(f"<======== Welcome {username} =========>")
+    print("1. Start Typing Test")
+    print("2. Show Leaderboard")
+    print("3. Quit")
+    
+    choice = input(f"{CYAN}Choice: {END_COLOR}")
+    return username, choice
+  except Exception as e: 
+        print(f"{RED}Something went wrong{END_COLOR}")
+  except KeyboardInterrupt:
+        print("Exiting...")
+        
+def display_category_data(category):
+    print(f"{PURPLE}{words_dict[category]['paragraph']}{END_COLOR}")
