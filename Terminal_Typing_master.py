da6315031c990525de4da20c775be6cadaef2c69
arpitkesharwani77 <arpitkesharwani77@gmail.com>
2024-01-08 15:29:09 +0530

    update the leaderboard

--- Terminal_Typing_master.py
+++ Terminal_Typing_master.py
@@ -18,3 +18,22 @@ def load_words_from_json(filename):
 # Load categories data from an external file
 categories_data_filename = 'words.json'
 words_dict = load_words_from_json(categories_data_filename)
+
+# Update leaderboard 
+def update_leaderboard(username, words_typed, time_taken, wpm):
+    leaderboard_file = 'leaderboard.json'
+    
+    try:
+        with open(leaderboard_file, 'r') as f:
+            leaderboard = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        leaderboard = {}
+
+    leaderboard[username] = {
+        f"Words Typed " : words_typed,
+        "Time Taken": f"{time_taken:.2f} seconds",
+        "Words Per Minute": f"{wpm} WPM "
+    }
+
+    with open(leaderboard_file, 'w') as f:
+        json.dump(leaderboard, f, indent=4)  
